ed9e86978d9f6b773419ab142429dc1535b8896a
jsh dncn <pythonicqualms@gmail.com>
2026-02-17 05:48:12 -0600

    We dont need no terraform

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-# ABOUTME: S3 backend for Terraform remote state storage.
-# ABOUTME: Update bucket and dynamodb_table to match your existing state infrastructure.
-
-terraform {
-  backend "s3" {
-    use_lockfile = true
-  }
-}
